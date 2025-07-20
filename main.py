187c764f17e089fdfe387f25ce54cebb5be52192
Emir Özbir <emirozbir@Emir-MacBook-Air.local>
2025-07-20 16:02:42 +0300

    main: Lint check

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import json
+from fastapi import FastAPI, Response, HTTPException, status
+from fastapi.responses import JSONResponse
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 from src.embed import Embedder
 from src.consts import LLM_SCORE_LIST
 from src.base_models import LLMResponse, GoldenDataList
 from src.metrics import LLM_GRADE_SCORE
-from fastapi import FastAPI, Response, HTTPException, status
-from fastapi.responses import JSONResponse
-from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 
 app = FastAPI()
 embedding_service = Embedder()
@@ -13,7 +13,11 @@ embedding_service = Embedder()
 @app.post("/api/v1/answers")
 async def update_item(item: LLMResponse):
     try:
-        embedding_service.redis_cli.rpush(LLM_SCORE_LIST, json.dumps({"id":item.id, "llm_output": item.llm_output}))        
+        embedding_service.redis_cli.rpush(
+            LLM_SCORE_LIST,
+            json.dumps({"id":item.id, "llm_output": item.llm_output})
+        )
+
         return JSONResponse(
             content={"message": "LLM output successfully updated."},
             status_code=status.HTTP_200_OK
@@ -39,7 +43,8 @@ async def embed_structured_golden_answers(item: GoldenDataList):
 async def metrics():
     try:
         _llm_output_list = embedding_service.redis_cli.lrange(LLM_SCORE_LIST, 0, -1)
-        grade_results = embedding_service._grade_llm_outputs(llm_outputs=[json.loads(item) for item in _llm_output_list])
+        grade_results = embedding_service._grade_llm_outputs(
+            llm_outputs=[json.loads(item) for item in _llm_output_list])
         for result in grade_results:
             LLM_GRADE_SCORE.labels(
                 qid=result["id"],
